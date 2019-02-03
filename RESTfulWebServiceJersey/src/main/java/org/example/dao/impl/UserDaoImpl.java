6ef59e5e0c0eef487623211b6f9e555c120ac6d5
Edward Rosenberg <edward.rosenberg@outlook.com>
2019-02-02 21:08:56 -0600

    spring framework version update, project structure clean up

--- RESTfulWebServiceJersey/src/main/java/org/example/dao/impl/UserDaoImpl.java
+++ RESTfulWebServiceJersey/src/main/java/org/example/dao/impl/UserDaoImpl.java
@@ -14,6 +14,7 @@ import org.example.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+
 @Component("userDao")
 public class UserDaoImpl implements UserDao {
 
